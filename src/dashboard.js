26e507bf357b875696317b2a6559dece3263e169
Wilson <wilsonfzheng@gmail.com>
2025-01-10 16:08:47 -0500

    placeholder for dashboard

---
+++ src/dashboard.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const dashboard = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Welcome to your MLB dashboard!</p>
+        </div>
+    )
+}
+
+export default dashboard
