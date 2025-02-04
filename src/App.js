3eda953af613b5ef671a77da46b4bc411d929251
Wilson <wilsonfzheng@gmail.com>
2025-02-04 17:00:51 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import SignupForm from "./SignupForm";
 import LoginForm from './LoginForm';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from "./dashboard";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
-      <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<SignupForm />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-      </BrowserRouter>
+    <BrowserRouter basename="/MLB-StatHub">
+      <Routes>
+        <Route path="/" element={<SignupForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
